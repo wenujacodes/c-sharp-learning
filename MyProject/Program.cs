f411dea94b7abc15dac6f04e3bc406afa97b2714
“wenujacodes” <wenujaweb@gmail.com>
2026-07-06 21:13:52 +0530

    unit-4 update

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,4 +1,18 @@
-﻿// Console.WriteLine("Hello World!");
+﻿//!unit-2
+
+// Console.WriteLine("Hello World!");
+
+
+//!unit-3
 
 Console.WriteLine("Congratulations!");
-Console.Write("You worte your first lines of code.");
+//?Class | Method | Literal string
+//?A Statement
+
+// Console.Write("You worte your first lines of code.");
+
+
+//!unit-4
+
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
